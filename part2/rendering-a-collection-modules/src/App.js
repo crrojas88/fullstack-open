44c79c16a873d681f87b1e1cd2f7596abb595a2a
crrojas88 <carlorrojas@gmail.com>
2021-04-15 11:48:38 -0700

    completed exercise 2.1 - refactored code in order to have multiple components.

---
+++ part2/rendering-a-collection-modules/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Course from './components/Course';
+
+// const Total = (props) => {
+//   return(
+//     <p>
+//       Total Number of exercises: {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
+//     </p>
+//   )
+// }
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

ce994d1cd80c6e7d29cf7ead6c2f565f35b14868
crrojas88 <carlorrojas@gmail.com>
2021-04-20 21:52:11 -0700

    completed 2.11 - implemented axios library to fetch from server

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Search from './components/Search';
 import Form from './components/Form';
 import ContactList from './components/Contact-list';
 
 const App = () => {
-
-  // added id keys to contacts. 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+ 
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchField, setSearchField ] = useState('')
+
+  // fetch promise and data from db.json
+  const hook = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log(response)
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
 
   // Handles change for different input values.
   const handleChange = (e) => {

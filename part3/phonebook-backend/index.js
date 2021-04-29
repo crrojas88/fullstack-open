d7d16debcf6aca6c888daa3dba099e655ff725cc
crrojas88 <carlorrojas@gmail.com>
2021-04-29 14:32:40 -0700

    completed 3.8 - modified morgan to log data from HTTP POST.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms :body :req[content]'))
+
+// app.use(morgan('tiny'))
 
 let persons = [
     {

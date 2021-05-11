24eac27be3d3507412fe20bc8de1a3df5b074e3c
crrojas88 <carlorrojas@gmail.com>
2021-05-11 16:25:30 -0700

    Completed exercise 3.9 -  installed cors middleware to communicate front end and back end with exception of edit functionality

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms :body :req[content]'))
 
-// app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
     {

ca8fc40d838f2c55530540c8204e8125228c119f
crrojas88 <carlorrojas@gmail.com>
2021-06-09 14:43:09 -0700

    completed using db in route handlers

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { response, request } = require('express')
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
+
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -44,7 +46,10 @@ let notes = [
   })
 
   app.get('/api/notes', (request, response) => {
-      response.json(notes)
+      Note.find({})
+      .then(notes => {
+          response.json(notes)
+      })
   })
 
   const generateId = () => {
@@ -55,32 +60,29 @@ let notes = [
   app.post('/api/notes', (request, response) => {
       const body = request.body
 
-      if(!body.content) {
+      if(body.content === undefined) {
           return response.status(400).json({
               error: 'content missing'
           })
       }
 
-      const note = {
+      const note = new Note({
           content: body.content,
           important: body.important || false,
           date: new Date(),
-          id: generateId(),
-      }
+      })
 
-      notes = notes.concat(note)
-      
-      response.json(note)
+      note.save()
+      .then(savedNote => {
+        response.json(savedNote)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-      const id = Number(request.params.id)
-      const note = notes.find(note => note.id === id)
-      if(note) {
+      Note.findById(request.params.id)
+      .then(note => {
           response.json(note)
-      } else {
-          response.status(404).end()
-      }
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -96,7 +98,7 @@ let notes = [
 
   app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

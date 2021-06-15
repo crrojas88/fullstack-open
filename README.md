4a749cb8e4b38939ab44de04e76f9c3a5f3b9b9e
crrojas88 <carlorrojas@gmail.com>
2021-06-14 19:34:59 -0700

    new commit on master

---
+++ README.md
@@ -0,0 +1,28 @@
+# The Phonebook Backend
+<br />
+
+### Solution for Part 3 of the Fullstack Open Course
+<br />
+
+### Deployed server/app
+
+You can find the deployed app and server [here](https://phonebook-backend-2021.herokuapp.com/)
+
+<br />
+
+### Deploying from a subdirectory
+Assuming you've already installed the Heroku CLI, are logged in, and created a new Heroku app, use the following commands in order to deploy a subdirectory to Heroku from the CLI. Make sure to commit your code first.
+<br />
+
+
+```
+heroku git:remote -a phonebook-backend-2021
+```
+CD to the top level of the working tree
+```
+cd ..
+```
+Specify the path/to/subdirectory
+```
+git subtree push --prefix part3/phonebook-backend heroku main
+```
---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://rodrigo:${password}@cluster0.432zh.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose
+.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+// If length of CLI arguments are equal to 5 then they are added to db, else if they equal to 3, all results get printed.
+// If length of CLI arguments is 4 or more than 5, you have to include quotations. 
+if(process.argv.length === 5){
+    person.save()
+    .then(result => {
+    console.log(`Added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+})
+} else if(process.argv.length === 3) {
+    Person.find({})
+    .then(result => {
+        console.log('Phonebook:')
+        result.forEach(person => {
+            console.log(person.name, ' - ', person.number)
+        })
+        mongoose.connection.close()
+    })
+} else if(process.argv.length === 4 || process.argv.length > 5) {
+    console.log('Wrong number of arguments. If your entries have spaces, please wrap them in quotes.')
+    mongoose.connection.close()
+}
